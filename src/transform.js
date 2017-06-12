cbb9e9b4ac60c82c7477ee199f1f60356cdec67c
Daniel Bushman <daniel@sngsft.com>
2017-06-12 13:21:40 -0600

    mapValues in URI where it belongs

--- src/transform.js
+++ src/transform.js
@@ -2,18 +2,6 @@ var R = require('ramda');
 var Immutable = require('immutable');
 var URI = require('./URI.js');
 
-function mapValues(value) {
-  switch(R.type(value)) {
-    case 'String':
-      return URI(value)(obj);
-    case 'Object':
-      return transform(value, obj);
-    case 'Array':
-      return value.map(item => {
-        return transform(item, obj);
-      });
-  }
-}
 
 module.exports = function transform (template, obj) {
   if (R.type(obj) != 'Object') { return; }
@@ -22,5 +10,5 @@ module.exports = function transform (template, obj) {
     return URI(template)(obj)(i);
   }
 
-  return R.map(mapValues, template);
+  return R.map(URI.mapValues, template);
 };
