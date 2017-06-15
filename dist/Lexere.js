09b60e898789c3b8308efc1a7ff3e175a28b9073
Daniel Bushman <daniel.bushman@singularity.software>
2017-06-14 19:27:44 -0600

    lex via streams? what?

---
+++ dist/Lexere.js
@@ -0,0 +1,3 @@
+// " input character stream"
+// could lexing via streams allow for parellel lexing, only blocking as necessary? Could I lex an entire template non-sequentially?
+'Λ a thing, I once.. heard';
